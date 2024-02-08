f84130bc8773a0c35394c11cf976ec5ebedd23d6
Sathyanarayanan-SK <135942886+Sathyanarayanan-SK@users.noreply.github.com>
2024-02-08 16:40:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "do-not-delete-sathya-terraform-backend"                   #replace with the project specific bucketname
-    key    = "backend/sathya/terraform.tfstate"
-    region = "ap-south-1"                  #replce the region
-    dynamodb_table = "terraform-state-lock-DO-NOT-DELETE-sathya"          #do not change the dynamoDB table name
-  }
-}
+
